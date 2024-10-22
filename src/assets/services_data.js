9183928ba77a321e42b5db53ac99c6fd78c51416
Arpit9026 <arpitkairati07@gmail.com>
2024-10-22 23:39:32 +0530

    key changes

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,34 @@
 const Services_Data = [
     {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "01",
+        s_name: "Web Development",
+        s_desc: "Building responsive and user-friendly websites for various applications."
     },
     {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "02",
+        s_name: "Machine Learning & AI",
+        s_desc: "Creating algorithms to enable machines to learn from data and make predictions."
     },
     {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "03",
+        s_name: "UI/UX Design",
+        s_desc: "Designing intuitive interfaces and experiences for enhanced user engagement."
     },
     {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "04",
+        s_name: "Data Science and Analytics",
+        s_desc: "Extracting insights from data to support data-driven decision-making."
     },
     {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "05",
+        s_name: "Data Engineering",
+        s_desc: "Building systems for data collection, storage, and processing."
     },
     {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "06",
+        s_name: "Technical Writing & Documentation",
+        s_desc: "Creating clear documentation to help users understand software and systems."
     },
-]
- 
+];
+
 export default Services_Data;
