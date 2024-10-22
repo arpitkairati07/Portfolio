640d4f43e108ecf0d640744f969ad1a515953c0f
Arpit9026 <arpitkairati07@gmail.com>
2024-10-23 00:59:09 +0530

    ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Hero.css';
 // import myPhoto from './../assets/my_photo.jpg'; // Import your profile image
-import top from '././../assets/top.png';
+import my_photo from '././../assets/my_photo.jpg';
 import AnchorLink from 'react-anchor-link-smooth-scroll'; 
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-      <img src={top} alt="Top Image" className='circular-image' /> {/* Use imported variable */}
+      <img src={my_photo} alt="Top Image" className='circular-image' /> {/* Use imported variable */}
       <h1>I'm Arpit Kairati</h1>
       <h2>Software Developer</h2>
       <p>A versatile software developer skilled in React JS, Node JS, Python, and Java with a passion for building dynamic and efficient applications.</p>
