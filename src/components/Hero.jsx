7bfa24180354e60b10fcb346fcd9ac0cff79cc50
Arpit9026 <arpitkairati07@gmail.com>
2024-10-23 00:22:13 +0530

    ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-      <img src="src/assets/mainhead.png" alt="Profile" className='circular-image'/>
+      <img src="src/assets/top.png" alt="Profile" className='circular-image'/>
       <h1>I'm Arpit Kairati</h1>
       <h2>Software Developer</h2>
       <p>A versatile software developer skilled in React JS, Node JS, Python, and Java with a passion for building dynamic and efficient applications.</p>
