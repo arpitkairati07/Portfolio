77937afb6544ec88163386481513ffc61f10f649
Arpit9026 <arpitkairati07@gmail.com>
2024-11-12 01:54:26 +0530

    add functionality to go project

--- src/components/My_Work/My_Work.jsx
+++ src/components/My_Work/My_Work.jsx
@@ -7,21 +7,25 @@ import arrow from '../../assets/arrow_icon.svg';
 const My_Work = () => {
   return (
     <div id='work' className='mywork'>
-        <div className="mywork-title">
-            <h1>My Latest Work</h1>
-            <img src={theme} alt="Theme" />
-        </div>
-        <div className="mywork-container">
-        {mywork_data.map((work,index)=>{
-            return <img  key={index} src={work.w_img} alt="" />
-        })} 
-        </div>
-        <div className="mywork-showmore">
-            <p>Show More</p>
-            <img src={arrow} alt="" />
-        </div>
+      <div className="mywork-title">
+        <h1>My Latest Work</h1>
+        <img src={theme} alt="Theme" />
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => {
+          return (
+            <a key={index} href={work.w_link} target="_blank" rel="noopener noreferrer">
+              <img src={work.w_img} alt="Project Thumbnail" />
+            </a>
+          );
+        })}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow} alt="" />
+      </div>
     </div>
   )
 }
 
-export default My_Work
+export default My_Work;
